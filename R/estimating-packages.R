6a92c3b0ca13804ba98cb7573eb52d7027a93b16
David Maybury <david@maybury.ca>
2020-08-25 16:06:35 -0400

    Initial build.

---
+++ R/estimating-packages.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+#' @import tidyr
+#' @import purrr
+#' @import dplyr
+#' @import ggplot2
+#' @import scales
+#' @import R2jags
+#' @import choroplethr
+#' @import choroplethrMaps
+#' @import lubridate
+"_PACKAGE"
